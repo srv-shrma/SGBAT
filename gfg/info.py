71f0b45b5f106f127c901ba2e3f17ea6848751b6
User <finalyear.ml@gmail.com>
2022-07-07 18:37:40 +0530

    s

--- gfg/info.py
+++
@@ -1,5 +0,0 @@
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com' #service
-EMAIL_HOST_USER = 'sgbar4@gmail.com'
-EMAIL_HOST_PASSWORD = 'herit@44'
-EMAIL_PORT = 587
--- showstatic/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShowstaticConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'showstatic'
